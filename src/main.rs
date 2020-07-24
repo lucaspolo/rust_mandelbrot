44ce84093c3f5a879ca539a2122d662d490f8d9b
Lucas Polo <pradopolo@gmail.com>
2020-07-23 21:55:48 -0300

    Invert colors

--- src/main.rs
+++ src/main.rs
@@ -119,8 +119,8 @@ fn render(
         for column in 0 .. bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
-                None => 0,
-                Some(count) => 255 - count as u8
+                None => 255,
+                Some(count) => count as u8
             };
         }
     }
