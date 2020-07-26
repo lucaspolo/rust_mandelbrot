206165679c2581aba0924e68ec86c5121235ad26
Lucas Polo <pradopolo@gmail.com>
2020-07-25 22:52:40 -0300

    Apply formatation

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,14 @@ fn main() {
         writeln!(
             std::io::stderr(),
             "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERLEFT"
-        ).unwrap();
+        )
+        .unwrap();
         writeln!(
             std::io::stderr(),
             "Example: {} mandel.png 100x750 -1.20,0.35 -1,0.20",
             args[0]
-        ).unwrap();
+        )
+        .unwrap();
         std::process::exit(1);
     }
 
@@ -40,7 +42,8 @@ fn main() {
                 let height = band.len() / bounds.0;
                 let band_bounds = (bounds.0, height);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
                 spawner.spawn(move || {
                     render(band, band_bounds, band_upper_left, band_lower_right);
@@ -70,12 +73,10 @@ use std::str::FromStr;
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None
-            }
-        }
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
     }
 }
 
@@ -93,13 +94,19 @@ fn test_parse_pair() {
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
-        None => None
+        None => None,
     }
 }
 
 #[test]
 fn test_parse_complex() {
-    assert_eq!(parse_complex("1.25,-0.0625"), Some( Complex { re: 1.25, im: -0.0625 }));
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
     assert_eq!(parse_complex(",123"), None);
 }
 
@@ -107,12 +114,15 @@ fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
     upper_left: Complex<f64>,
-    lower_right: Complex<f64>
+    lower_right: Complex<f64>,
 ) -> Complex<f64> {
-    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
     Complex {
         re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
     }
 }
 
@@ -120,10 +130,10 @@ fn pixel_to_point(
 fn test_pixel_to_point() {
     assert_eq!(
         pixel_to_point(
-            (100, 100), 
-            (25, 75), 
-            Complex { re: -1.0, im: 1.0 }, 
-            Complex { re: 1.0, im: -1.0}
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
         ),
         Complex { re: -0.5, im: -0.5 }
     )
@@ -133,16 +143,16 @@ fn render(
     pixels: &mut [u8],
     bounds: (usize, usize),
     upper_left: Complex<f64>,
-    lower_right: Complex<f64>
+    lower_right: Complex<f64>,
 ) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for row in 0 .. bounds.1 {
-        for column in 0 .. bounds.0 {
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
-                Some(count) => 255 - count as u8
+                Some(count) => 255 - count as u8,
             };
         }
     }
@@ -150,20 +160,25 @@ fn render(
 
 extern crate image;
 
-use image::ColorType;
 use image::png::PNGEncoder;
+use image::ColorType;
 use std::fs::File;
 
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
 
     let encoder = PNGEncoder::new(output);
 
     encoder.encode(
         &pixels,
-        bounds.0 as u32, bounds.1 as u32,
-        ColorType::Gray(8))?;
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
 
     Ok(())
-
-} 
+}
